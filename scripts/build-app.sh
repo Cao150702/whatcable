b0d822723c68d7312f8f0947b121c6a856406df6
Darryl Morley <darrylmorley.uk@gmail.com>
2026-05-05 18:22:02 +0100

    Bump version to 0.5.14 (build 28)

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -19,8 +19,8 @@ fi
 
 APP_NAME="WhatCable"
 BUNDLE_ID="com.bitmoor.whatcable"
-VERSION="0.5.13"
-BUILD_NUMBER="27"
+VERSION="0.5.14"
+BUILD_NUMBER="28"
 MIN_OS="14.0"
 CLI_PRODUCT="whatcable-cli"
 CLI_BIN_NAME="whatcable"
