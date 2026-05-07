d3e151ff7e226ebefdfeb840f857d5b24e339a26
Darryl Morley <darrylmorley.uk@gmail.com>
2026-05-07 17:39:08 +0100

    Bump version to 0.8.5 (build 38)

--- scripts/smoke-test.sh
+++ scripts/smoke-test.sh
@@ -23,8 +23,8 @@ fi
 
 APP_NAME="WhatCable"
 BUNDLE_ID="uk.whatcable.whatcable"
-VERSION="0.8.4"
-BUILD_NUMBER="37"
+VERSION="0.8.5"
+BUILD_NUMBER="38"
 MIN_OS="14.0"
 CLI_PRODUCT="whatcable-cli"
 CLI_BIN_NAME="whatcable"
